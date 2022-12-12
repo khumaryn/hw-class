a9dfa527d7a76acf5b418f4c99a41e34e33f1703
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-13 02:28:11 +0400

    Add files via upload

---
+++ classtask1/classtask1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace classtask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ededi 1 vahid artiran program
+            Console.WriteLine("ededi daxil edin");
+            int num=Convert.ToInt32(Console.ReadLine());
+            var resultartim = artim(num);
+            Console.WriteLine(resultartim);
+        }
+        static int artim(int num)
+        {
+            num++;
+            return num;
+        }
+    }
+}
