64a93f76faa0668e40a1230ea8c13919d0cec792
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-13 02:28:53 +0400

    Add files via upload

---
+++ classtask2/classtask2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace classtask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bize verilmis ad soyadi birlesdirsin
+            Console.WriteLine("adi ve soyadi daxil edin");
+            string name = Console.ReadLine();
+            string surname= Console.ReadLine();
+            var fullname = adsoyad(name,  surname);
+
+                Console.WriteLine(fullname);
+
+        }
+        static string adsoyad(string name, string surname)
+        {
+            var adsoyad = name + " " + surname;
+            return adsoyad;
+        }
+
+
+    }
+}
