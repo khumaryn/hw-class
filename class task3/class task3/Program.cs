7d0f223dae48c1af447fe39b02b0514a21936d96
khumaryn <119412655+khumaryn@users.noreply.github.com>
2022-12-13 02:29:27 +0400

    Add files via upload

---
+++ class task3/class task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace class_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //yazilani int-e cevirmek
+            Console.WriteLine("ededi daxil edin");
+            string num = Console.ReadLine();
+            resultnum = int_ecevirmek(number);
+            Console.WriteLine(num);
+
+
+            
+        }
+        static int int_ecevirmek(int number)
+        {
+            string num = Console.ReadLine();
+            return Convert.ToInt32 (num);
+        }
+    }     
+}
